931f95f6f994e6a7aea8e3a2cb08bc8a21e0f6c5
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-01-11 10:17:14 -0500

    Set up base structure

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.createElement("h1");e.textContent="Hello Test",document.body.appendChild(e)})();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const element = document.createElement('h1');
+
+element.textContent = 'Hello Test';
+
+document.body.appendChild(element);
