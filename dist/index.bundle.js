d22c54db35dec6b790fc525001e0a60161a83ed1
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-01-21 20:21:48 -0500

    Remove dist folder

--- dist/index.bundle.js
+++
@@ -1,6 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-// Your code goes here
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7O0FBQUEsc0IiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrdGVtcGxhdGUvLi9zcmMvc2NyaXB0cy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBZb3VyIGNvZGUgZ29lcyBoZXJlIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
